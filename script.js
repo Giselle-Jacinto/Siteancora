8126017fb5bbe710e5a05cbf2fa2d06e184e786e
Giselle Jacinto <gisellejacinto1219@gmail.com>
2023-09-20 15:30:56 -0300

    ativ 01 html

---
+++ script.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector(".nav");
+const btnButton = document.querySelector(".btn-menu");
+
+btnButton.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
